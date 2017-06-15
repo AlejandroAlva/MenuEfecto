ae2b676d0e3419965118e89604349513723a0439
Alejandro Alvarez (Frepo) <alexzander_alva9010@hotmail.com>
2017-06-15 13:30:30 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+              <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+              
+              <title>Menu</title>
+               
+              <link href="principal.css" rel="stylesheet" type="text/css" />
+       </head>
+
+<body>
+    	  <div class="container">
+                 <div class="header">
+                 	<a href="index.php">
+								<div class="contenedor" id="uno">								
+								<img class="icon" src="imagenes/icon1.png">								
+								<p class="texto">Titulo</p>																	
+								</div>
+								</a>
+							
+					
+								<a href="Servicios.php">
+								<div class="contenedor" id="dos">
+								<img class="icon" src="imagenes/icon5.png">
+								<p class="texto">Titulo</p>
+							</div>
+							</a>
+							
+						
+				
+							<div class="contenedor" id="tres">
+								<img class="icon" src="imagenes/icon2.png">
+								<p class="texto">Titulo</p>
+							</div>
+						
+
+					
+							<div class="contenedor" id="cuatro">
+	 							
+								<img class="icon" src="imagenes/icon6.png">
+								<p class="texto">Titulo</p>
+							</div>
+							
+						
+							<div class="contenedor" id="cinco">
+	 							
+								<img class="icon" src="imagenes/icon4.png">
+								<p class="texto">Titulo</p>
+							</div>
+                </div>
+        
+             <div class="sidebar1">
+             </div>
+
+           </div>
+           <div class="footer">
+          </div>
+      </div>
+  	
+  </body>
+</html>
